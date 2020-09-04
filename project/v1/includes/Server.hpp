eab73a46cce7426e09dbf0f32d4b53d9ee36432c
Fraberg <frcs.berger@gmail.com>
2020-09-04 19:14:44 +0200

    local

--- project/v1/includes/Server.hpp
+++ project/v1/includes/Server.hpp
@@ -30,25 +30,29 @@ std::vector<std::string> split(const std::string& str, char delim);
 
 class Server
 {
-     class Location 
-	 {
-         private:
-			Location();	 		
-         public:
-		 	Location(std::string uri, std::string root, std::string index, std::string method)
-		 	{
-				_uri = uri;
-				_root = root;
-				_index = index;
-				_method = split(method, ',');
-		 	};
-			
-			std::string 				_uri;
-			std::string 				_root;
-			std::string					_index;
-			std::vector<std::string>	_method;
-	 };
-	
+    /*
+    ** class
+    */
+
+    public:
+        class Location 
+        {
+            private:
+            Location();	 		
+            public:
+            Location(std::string uri, std::string root, std::string index, std::string method)
+            {
+                _uri = uri;
+                _root = root;
+                _index = index;
+                _method = split(method, ',');
+            };
+            
+            std::string 				_uri;
+            std::string 				_root;
+            std::string					_index;
+            std::vector<std::string>	_method;
+        };
 
     /*
     ** member variables
