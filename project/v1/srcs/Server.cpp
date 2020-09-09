74868be4c3e4379d01b6e3d916945934b27267c0
mac <mac@iMac-de-mac.local>
2020-09-09 16:10:09 +0200

    ajout de old dans les loc

--- project/v1/srcs/Server.cpp
+++ project/v1/srcs/Server.cpp
@@ -12,10 +12,10 @@ Server::Server(std::string serverName, int port):
 	// --- > location et error a definir dans le prasing de la conf
 	_error = "./www/error";
 	
-    Location *location1 = new Location("/", "./www", "index.html", "GET,POST,HEAD,OPTIONS");
+    Location *location1 = new Location("/", "./old/www", "index.html", "GET,POST,HEAD,OPTIONS");
 	_locations.push_back(location1);
 
-	Location *location2 = new Location("/test", "./www/test", "index.html", "DELETE");
+	Location *location2 = new Location("/old/test", "./www/test", "index.html", "DELETE");
 	_locations.push_back(location2);
 }
 
