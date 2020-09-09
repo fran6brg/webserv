8ee61036b995e29dff339b3d03aac5d4cd0f7bff
Fraberg <frcs.berger@gmail.com>
2020-09-09 15:39:17 +0200

    add www

--- project/v1/test.py
+++ project/v1/test.py
@@ -82,10 +82,10 @@ printResponse(r)
 
 # PUT -------------------------------------------------------------------------
 
-# body = "Some random text to see if put is working"
-# r = requests.put('http://localhost:8080/test/put.txt', data=body)
-# printResponse(r)
-# prettyPrint(r)
+body = "Some random text to see if put is working"
+r = requests.put('http://localhost:8080/test/put.txt', data=body)
+printResponse(r)
+prettyPrint(r)
 
 # body = "Some5text5to5test"
 # r = requests.put('http://localhost:8080/put_test/testfile.txt', data=body)
