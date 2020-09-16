cfcbc7e6e7210bd6f2a00355ce72f66c564bc00a
Fraberg <frcs.berger@gmail.com>
2020-09-16 17:23:28 +0200

    local

--- project/v1/srcs/methods/put.cpp
+++ project/v1/srcs/methods/put.cpp
@@ -3,19 +3,20 @@
 void			Response::put(Request *req)
 {
 	LOG_WRT(Logger::DEBUG, "inside put\n");
+
     if (req->_content_length == -1)
     {
-	    // std::cout << "inside put: BAD_REQUEST_400" << std::endl;
+	    LOG_WRT(Logger::DEBUG, "inside put: BAD_REQUEST_400");
         _status_code = BAD_REQUEST_400;
         return ;
     }
 
-    // if (req->_content_length != req->_body_length)
-    // {
-	//     std::cout << "inside put: REQUEST_ENTITY_TOO_LARGE_413: " << req->_content_length << " vs " <<  req->_body_length << std::endl;
-    //     _status_code = REQUEST_ENTITY_TOO_LARGE_413;
-    //     return ;
-    // }
+    if (req->_content_length != req->_body_length)
+    {
+	    LOG_WRT(Logger::DEBUG, "inside put: REQUEST_ENTITY_TOO_LARGE_413: " + std::to_string(req->_content_length) + " vs " +  std::to_string(req->_body_length));
+        _status_code = REQUEST_ENTITY_TOO_LARGE_413;
+        return ;
+    }
 
 	std::ifstream f1(req->_file);
 	if (f1.good()) // test if _file already exists
@@ -38,7 +39,7 @@ void			Response::put(Request *req)
 		    LOG_WRT(Logger::DEBUG, "put: writing req->body inside _file\n");
 			f2 << req->_body[0].second.substr(0, req->_content_length) << std::endl;
 		}
-        else
+        else // simple text, no query string formated
 		{
 			LOG_WRT(Logger::DEBUG, "put: writing req->text_body inside _file\n");
 			f2 << req->_text_body << std::endl;
