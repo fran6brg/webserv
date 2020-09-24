5adf1c45c103dbbe343dabcf896e90f9149aef5e
Fraberg <frcs.berger@gmail.com>
2020-09-25 00:12:03 +0200

    tests

--- project/v1/push.sh
+++ project/v1/push.sh
@@ -15,7 +15,7 @@ git add .
 echo "git commit -m $1"
 git commit -m $1
 
-read -p "Push ? \\n> " -n 1 -r
+read -p "Push ? \n> " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
